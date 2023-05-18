c7f3016390b6eed129ff2fcafb64607abc615bed
ZentexGG <stefan.tanase2121@gmail.com>
2023-05-18 18:07:23 +0300

    Created context & updated folder structure for data layer

---
+++ backend/DataLayer/Data/CybermartContext.cs
@@ -0,0 +1,14 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer.Data;
+
+public class CybermartContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"");
+    }
+}
