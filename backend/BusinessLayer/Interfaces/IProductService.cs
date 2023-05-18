f6a5c52fb85803760adeb8c4cfdaf8cc6f95bc65
ZentexGG <stefan.tanase2121@gmail.com>
2023-05-18 23:46:23 +0300

    Product Service & Product Controller created & GetAllProducts working

---
+++ backend/BusinessLayer/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using DataLayer.Models;
+
+namespace BusinessLayer.Interfaces;
+
+public interface IProductService
+{
+    IEnumerable<Product> GetAllProducts();
+    IEnumerable<Product> GetFirstNumberOfProducts(int productsNum);
+    Product? GetProductByID(int id);
+    string AddProduct(Product product);
+    string UpdateProduct(int id, Product product);
+    string DeleteProduct(int id);
+}
