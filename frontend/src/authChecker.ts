99bf1bc9f5b38b2a06572e3b4ca00c1a9cbc314e
ZentexGG <stefan.tanase2121@gmail.com>
2023-07-13 13:15:49 +0300

    changed authChecker to async syntax

--- frontend/src/authChecker.ts
+++ frontend/src/authChecker.ts
@@ -1,17 +1,15 @@
 import jwt_decode from "jwt-decode";
+import { DecodedToken } from "./types";
 
-interface DecodedToken {
-  [key: string]: any;
-}
 
 const getCookie = (name: string) => {
   const cookieValue = document.cookie.match(`(^|;)\\s*${name}\\s*=\\s*([^;]+)`);
   return cookieValue ? cookieValue.pop() : "";
 };
-const checkAuth = () => {
+const checkAuth = async () => {
   const token = getCookie("token");
   if (token) {
-    const decodedCookie: DecodedToken = jwt_decode(token);
+    const decodedCookie: DecodedToken = await jwt_decode(token);
     return decodedCookie;
   }
   return false;
