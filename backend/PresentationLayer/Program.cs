8c9356e79320934e93205c1743eeccb2443c2777
daniekk <alexandrudanielaka47@gmail.com>
2023-07-13 14:22:56 +0300

    da

--- backend/PresentationLayer/Program.cs
+++ backend/PresentationLayer/Program.cs
@@ -92,6 +92,7 @@ builder.Services.Configure<DataProtectionTokenProviderOptions>(
 
 // Add services to the container.
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddControllers();
 
