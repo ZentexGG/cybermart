79ac476c15d0388d4cb01ecc58a0543dd6f09af6
ZentexGG <stefan.tanase2121@gmail.com>
2023-07-15 16:25:53 +0300

    changed login to orders button

--- frontend/src/Components/LoggedInUserDropwdown/LoggedInUserDropdown.tsx
+++ frontend/src/Components/LoggedInUserDropwdown/LoggedInUserDropdown.tsx
@@ -60,7 +60,7 @@ export default function LoggedInUserDropdown({
               href="/login"
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
             >
-              Login
+              Orders
             </a>
           </li>
           <SignoutButton />
