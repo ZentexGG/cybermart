9d25c5f5237beb603fa480d4ef965ca94190de09
ZentexGG <stefan.tanase2121@gmail.com>
2023-07-17 14:15:52 +0300

    default date to DateTime.Now

--- backend/BusinessLayer/Model/ProductPhotoDto.cs
+++ backend/BusinessLayer/Model/ProductPhotoDto.cs
@@ -5,5 +5,5 @@ public class ProductPhotoDto
     public int Id { get; set; }
     public string FileName { get; set; }
     public byte[] ImageData { get; set; }
-    public DateTime UploadDate { get; set; }
+    public DateTime UploadDate { get; set; } = DateTime.Now;
 }
