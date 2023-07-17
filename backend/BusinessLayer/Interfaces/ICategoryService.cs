7e2c6bbb21a812aa28e8d17e552792b9122129eb
ZentexGG <stefan.tanase2121@gmail.com>
2023-07-17 14:16:52 +0300

    api for category

---
+++ backend/BusinessLayer/Interfaces/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using DataLayer.Entities;
+
+namespace BusinessLayer.Interfaces;
+
+public interface ICategoryService
+{
+    IEnumerable<Category> GetAll();
+    Task CreateCategory(Category category);
+}
