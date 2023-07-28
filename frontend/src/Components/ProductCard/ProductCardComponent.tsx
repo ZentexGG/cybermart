39eb428922d6bd799fca7959461835cadbc63983
ZentexGG <stefan.tanase2121@gmail.com>
2023-07-28 15:48:10 +0300

    edit product adding to cart

--- frontend/src/Components/ProductCard/ProductCardComponent.tsx
+++ frontend/src/Components/ProductCard/ProductCardComponent.tsx
@@ -1,16 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import { checkAuth } from "../../authChecker";
 import { CartItem, ProductDto } from "../../types";
+import { useState } from "react";
 
 export default function ProductCardComponent({
-  product, handleAddToCart
+  product,
+  handleAddToCart,
+  setShowAlert,
 }: {
-    product: ProductDto;
+  product: ProductDto;
     handleAddToCart: (product: CartItem) => void;
-  }) {
-  
+    setShowAlert: (showAlert: boolean) => void;
+}) {
   const navigate = useNavigate();
-  
+
   const addToCart = async () => {
     const loggedIn = await checkAuth();
     if (!loggedIn) {
@@ -18,19 +21,23 @@ export default function ProductCardComponent({
       return;
     }
     const image: string = `data:image/jpeg;base64,${
-            product.photos && product.photos.length > 0
-              ? product.photos[0].imageData
-              : ""
-          }`
+      product.photos && product.photos.length > 0
+        ? product.photos[0].imageData
+        : ""
+    }`;
     const cartProduct: CartItem = {
       id: `${product.id}`,
       img: image,
       name: product.name,
       price: product.price,
-      amount: 1
-    }
+      amount: 1,
+    };
     handleAddToCart(cartProduct);
-  }
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 3000);
+  };
   return (
     <div className="w-full max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-blue-100 dark:border-blue-200">
       <a href={`products/${product.id}`}>
@@ -56,7 +63,8 @@ export default function ProductCardComponent({
             className="w-5 h-5 text-yellow-300"
             fill="currentColor"
             viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
+            xmlns="http://www.w3.org/2000/svg"
+          >
             {/* SVG code for the first star */}
           </svg>
           {/* SVG code for the other stars */}
@@ -68,12 +76,12 @@ export default function ProductCardComponent({
           <span className="text-3xl font-bold text-gray-900 dark:text-black">
             $599
           </span>
-          <a
-            href="#"
+          <button
             onClick={addToCart}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+          >
             Add to cart
-          </a>
+          </button>
         </div>
       </div>
     </div>
