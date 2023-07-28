3c83a0d598f251006cdf13d20f98d5fb39cd3dd8
ZentexGG <stefan.tanase2121@gmail.com>
2023-07-28 15:49:32 +0300

    edit cart design

--- frontend/src/Components/ShoppingCart/ShoppingCartComponent.tsx
+++ frontend/src/Components/ShoppingCart/ShoppingCartComponent.tsx
@@ -54,38 +54,44 @@ const Cart: React.FC<CartProps> = ({
         onClick={(e) => e.stopPropagation()}
         className="bg-white w-[250px] h-full absolute right-0 overflow-y-scroll"
       >
-        <h1 className="bg-red-400 py-2 text-center text-white">Cart</h1>
+        <h1 className="bg-blue-100 py-2 text-center text-red-700 font-medium">Shopping Cart</h1>
         <div className="flex flex-col items-center px-2 py-4">
-          {cart.map((item) => (
-            <div
-              key={item.id}
-              className="text-center border-b-[3px] w-full mb-2 flex flex-col items-center"
-            >
-              <img
-                className="w-[100px] h-[100px]"
-                src={item.img}
-                alt={item.name}
-              />
-              <p className="text-white font-bold w-6 h-6 rounded-full bg-blue-700">
-                {item.amount}
-              </p>
-              <h3 className="text-[0.8rem]">{item.name}</h3>
-              <div className="flex items-center my-2">
-                <button onClick={() => handleRemoveFromCart(item.id)}>
-                  <AiOutlineMinusSquare className="text-[30px] text-gray-500" />
-                </button>
-                <p className="text-red-600 mx-2">
-                  {DollarUsd.format(item.price)}
+          {cart.length == 0 ? (
+            <p className="text-center text-red-700 font-normal">
+              Your cart is empty!
+              <br /> Start adding products to your cart and they will show up
+              here.
+            </p>
+          ) : (
+            cart.map((item) => (
+              <div
+                key={item.id}
+                className="text-center border-b-[3px] w-full mb-2 flex flex-col items-center"
+              >
+                <img
+                  className="w-[100px] h-[100px]"
+                  src={item.img}
+                  alt={item.name}
+                />
+                <p className="text-white font-bold w-6 h-6 rounded-full bg-blue-700">
+                  {item.amount}
                 </p>
-                <button onClick={() => handleAddToCart(item)}>
-                  <AiOutlinePlusSquare className="text-[30px] text-gray-500" />
-                </button>
+                <h3 className="text-[0.8rem]">{item.name}</h3>
+                <div className="flex items-center my-2">
+                  <button onClick={() => handleRemoveFromCart(item.id)}>
+                    <AiOutlineMinusSquare className="text-[30px] text-gray-500" />
+                  </button>
+                  <p className="text-red-600 mx-2">
+                    {DollarUsd.format(item.price)}
+                  </p>
+                  <button onClick={() => handleAddToCart(item)}>
+                    <AiOutlinePlusSquare className="text-[30px] text-gray-500" />
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-          {cart.length > 0 && (
-            <p>Total: {DollarUsd.format(total(cart))}</p>
+            ))
           )}
+          {cart.length > 0 && <p>Total: {DollarUsd.format(total(cart))}</p>}
         </div>
       </div>
     </div>
