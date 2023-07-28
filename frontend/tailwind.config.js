aa683280247b11cf7cd16b8f5de4f054bc4354b6
ZentexGG <stefan.tanase2121@gmail.com>
2023-07-28 15:47:27 +0300

    animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,25 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-        keyframes: {
-            'fade-in-down': {
-                '0%': {
-                    opacity: '0',
-                    transform: 'translateX(-10px)'
-                },
-                '100%': {
-                    opacity: '1',
-                    transform: 'translateX(0)'
-                },
-            },
+      keyframes: {
+        "fade-in-down-out": {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(-10px)",
+          },
+          "50%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+          "100%": {
+            opacity: "0",
+          },
         },
-        animation: {
-            'fade-in-down': 'fade-in-down 0.5s ease-out',
-        },
+      },
+      animation: {
+        "fade-in-down-out": "fade-in-down-out 3s ease-out",
+      },
     },
   },
   plugins: [],
