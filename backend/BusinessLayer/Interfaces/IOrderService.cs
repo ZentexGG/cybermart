d7ba2bab72cb40a4bfa25317e06f74f211a3a1b8
daniekk12 <99326894+daniekk12@users.noreply.github.com>
2023-07-31 11:55:48 +0300

    OrderService Interface created

--- backend/BusinessLayer/Interfaces/IOrderService.cs
+++ backend/BusinessLayer/Interfaces/IOrderService.cs
@@ -7,7 +7,8 @@ namespace BusinessLayer.Service
 {
     public interface IOrderService
     {
-        Task<IEnumerable<Order>> GetOrdersByUser(int userId);
+        Task<Order> GetOrdersByOrderId(int userId);
+        Task<IEnumerable<Order>> GetOrdersByUser(int orderId);
         Task<IEnumerable<Order>> GetAllOrders();
         Task AddOrder(OrderDTO newOrder);
         Task RemoveOrder(int orderId);
