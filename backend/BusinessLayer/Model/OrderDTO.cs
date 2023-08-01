99b24b475ba7988be98d54b4b88bf7e6ff1fe07b
daniekk12 <99326894+daniekk12@users.noreply.github.com>
2023-08-01 06:33:53 +0300

    Added required to figure out error

--- backend/BusinessLayer/Model/OrderDTO.cs
+++ backend/BusinessLayer/Model/OrderDTO.cs
@@ -1,20 +1,40 @@
-﻿using DataLayer.Entities;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace BusinessLayer.Model;
-
-public class OrderDTO
+namespace BusinessLayer.Model
 {
-    public int Id { get; set; }
-    public List<OrderProductsDto> OrderProducts { get; set; }
-    public string FirstName { get; set; }
-    public string LasName { get; set; }
-    public bool CardPayment { get; set; }
-    public int UserId { get; set; }
-    public string Address { get; set; }
-    public string City { get; set; }
-    public string? Region { get; set; }
-    public string? PostalCode { get; set; }
-    public string Country { get; set; }
-    public string PhoneNumber { get; set; }
+    public class OrderDTO
+    {
+        [Required(ErrorMessage = "OrderProducts is required.")]
+        public List<OrderProductsDto> OrderProducts { get; set; }
 
+        [Required(ErrorMessage = "FirstName is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "CardPayment is required.")]
+        public bool CardPayment { get; set; }
+
+        [Required(ErrorMessage = "UserId is required.")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        public string City { get; set; }
+
+        // Region and PostalCode are optional, so we don't use Required here.
+        public string? Region { get; set; }
+
+        public string? PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Country is required.")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "PhoneNumber is required.")]
+        public string PhoneNumber { get; set; }
+    }
 }
