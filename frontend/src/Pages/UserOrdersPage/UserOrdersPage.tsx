ce9c331764cc86e2ae5b7649129bb50c3b277f63
daniekk12 <99326894+daniekk12@users.noreply.github.com>
2023-08-10 17:01:26 +0300

    UserOrder Page and Component

---
+++ frontend/src/Pages/UserOrdersPage/UserOrdersPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import UserOrdersComponent from '../../Components/UserOrdersComponent/UserOrdersComponent'
+
+export default function UserOrdersPage() {
+  return (
+    <UserOrdersComponent/>
+  )
+}
