985fd2e10ca716f33ed47e49ff56f5ba2b6afadb
ZentexGG <stefan.tanase2121@gmail.com>
2023-08-16 10:39:44 +0300

    user service updates identity user

--- backend/BusinessLayer/Interfaces/IUserService.cs
+++ backend/BusinessLayer/Interfaces/IUserService.cs
@@ -1,12 +1,16 @@
-﻿using BusinessLayer.Model;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using BusinessLayer.Model;
 using DataLayer.Entities;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 
 namespace BusinessLayer.Interfaces;
 
 public interface IUserService
 {
     Task<UserDto> GetUser(string email);
-    Task UpdateUser(UserDto rUserDto, IFormFile photo);
+    Task<List<Claim>> UpdateUser(UserDto rUserDto, IFormFile photo);
     Task CreateUserAsync(User user);
+    JwtSecurityToken GetToken(List<Claim> authClaims, bool rememberMe);
 }
--- backend/BusinessLayer/Service/UserService.cs
+++ backend/BusinessLayer/Service/UserService.cs
@@ -1,21 +1,33 @@
-﻿using System.Text.Json;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using BusinessLayer.Interfaces;
 using BusinessLayer.Model;
 using DataLayer.ContextInterface;
 using DataLayer.Entities;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BusinessLayer.Service;
 
 public class UserService : IUserService
 {
     private readonly IDbContext _context;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly IEmailService _emailService ;
+    private readonly IConfiguration _configuration;
 
-    public UserService(IDbContext context)
+    public UserService(IDbContext context, UserManager<IdentityUser> userManager, IEmailService emailService, IConfiguration configuration)
     {
+        _userManager = userManager;
         _context = context;
+        _emailService = emailService;
+        _configuration = configuration;
     }
 
     public async Task<UserDto> GetUser(string email)
@@ -37,7 +49,7 @@ public class UserService : IUserService
         return userDto;
     }
 
-    public async Task UpdateUser(UserDto rUserDto, IFormFile photo)
+    public async Task<List<Claim>> UpdateUser(UserDto rUserDto, IFormFile photo)
     {
         try
         {
@@ -50,6 +62,13 @@ public class UserService : IUserService
             {
                 // Update user properties from UserDto
                 user.Username = rUserDto.Username;
+                user.Email = rUserDto.Email;
+                var identityUser = await _userManager.FindByEmailAsync(user.Email);
+                identityUser.UserName = user.Username;
+                identityUser.NormalizedUserName = user.Username.ToUpper();
+                identityUser.Email = user.Email;
+                identityUser.NormalizedEmail = user.Email.ToUpper();
+                await _context.SaveChangesAsync();
 
                 if (photo != null)
                 {
@@ -75,9 +94,27 @@ public class UserService : IUserService
                         };
                         _context.UserPhotos.Add(userPhoto);
                     }
+                    
+                }
+                await _context.SaveChangesAsync();
+                var authClaims = new List<Claim>
+                {
+                    new("id", user.ID.ToString()),
+                    new("name", identityUser.UserName),
+                    new("email", identityUser.Email),
+                    new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+
+                var userRoles = await _userManager.GetRolesAsync(identityUser);
+        
+
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim("role",userRole));
                 }
 
-                await _context.SaveChangesAsync();
+                return authClaims;
             }
             else
             {
@@ -98,5 +135,20 @@ public class UserService : IUserService
     {
         await _context.Users.AddAsync(user);
         await _context.SaveChangesAsync();
+    }
+    
+    public JwtSecurityToken GetToken(List<Claim> authClaims, bool rememberMe)
+    {
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+        var expireTime = rememberMe ? DateTime.Now.AddDays(30) : DateTime.Now.AddHours(1);
+        JwtSecurityToken token = new JwtSecurityToken(
+            issuer: _configuration["JWT:ValidIssuer"],
+            audience: _configuration["JWT:ValidAudience"],
+            expires: expireTime,
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256));
+
+        return token;
     }
 }
