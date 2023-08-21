5c85bac80f2d971e9954d3cf8c576f18a424594e
ZentexGG <stefan.tanase2121@gmail.com>
2023-08-21 12:49:25 +0300

    create product page only accessible by admin

--- frontend/src/Pages/CreateProductPage/CreateProductPage.tsx
+++ frontend/src/Pages/CreateProductPage/CreateProductPage.tsx
@@ -1,10 +1,23 @@
 import { useEffect, useState } from "react";
 import CreateProductForm from "../../Components/CreateProductForm/CreateProductForm";
-import { Category } from "../../types";
+import { Category, DecodedToken } from "../../types";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { checkAuth } from "../../authChecker";
 
 export default function CreateProductPage() {
     const [categories, setCategories] = useState<Category[]>([]);
+    const navigate = useNavigate();
+    useEffect(() => {
+      const fetchUserInfo = async () => {
+        let userInfo = await checkAuth();
+        if (!userInfo || userInfo.role !== "Admin") {
+            navigate("/");
+            return;
+          }
+      };
+      fetchUserInfo();
+    }, []);
     useEffect(() => {
         const fetchCategories = async () => {
             const response = await axios.get("/api/categories");
