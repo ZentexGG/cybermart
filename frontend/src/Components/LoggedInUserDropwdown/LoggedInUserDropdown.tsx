c25894eb99d39ca3d17499c571bcc4741e02e4db
ZentexGG <stefan.tanase2121@gmail.com>
2023-11-02 17:04:45 +0200

    corected orders path

--- frontend/src/Components/LoggedInUserDropwdown/LoggedInUserDropdown.tsx
+++ frontend/src/Components/LoggedInUserDropwdown/LoggedInUserDropdown.tsx
@@ -68,7 +68,7 @@ export default function LoggedInUserDropdown({
           </li>
           <li>
             <a
-              href={`/UserOrders/${user?.id}`}
+              href={`/user-orders/${user?.id}`}
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
               Orders
             </a>
