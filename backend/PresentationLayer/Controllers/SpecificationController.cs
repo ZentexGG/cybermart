83b5afb0ef6e0ea0735bcbd5370b7f63e07bec75
ZentexGG <stefan.tanase2121@gmail.com>
2024-04-09 16:36:13 +0300

    put req for updating specs

--- backend/PresentationLayer/Controllers/SpecificationController.cs
+++ backend/PresentationLayer/Controllers/SpecificationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Interfaces;
 using BusinessLayer.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PresentationLayer.Controllers;
@@ -13,14 +14,15 @@ public class SpecificationController : ControllerBase
     {
         _service = service;
     }
-
+    
+    [Authorize(Roles = "Admin")]
     [HttpPost("{productId}")]
     public async Task<IActionResult> AddSpecsForProductId(int productId, List<SpecificationDto> specifications)
     {
         try
         {
             await _service.AddSpecsForProduct(productId, specifications);
-            return Ok();
+            return Ok(new { message = $"Successfully added specs for product ID: {productId}" });
 
         }
         catch (KeyNotFoundException e)
@@ -40,6 +42,24 @@ public class SpecificationController : ControllerBase
         {
             var result = await _service.GetSpecsForProduct(productId);
             return Ok(result);
+        }
+        catch (KeyNotFoundException e)
+        {
+            return NotFound(new { message = e.Message });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new { message = e.Message });
+        }
+    }
+
+    [HttpPut("{productId}")]
+    public async Task<IActionResult> UpdateSpecsForProduct(int productId, List<SpecificationDto> newSpecs)
+    {
+        try
+        {
+            await _service.ModifySpecsForProduct(productId, newSpecs);
+            return Ok(new { message = $"Successfully updated specs for product ID: {productId}" });
         }
         catch (KeyNotFoundException e)
         {
