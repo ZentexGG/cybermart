57c99956d5e37711bb733587481c7b3af450ebaf
ZentexGG <stefan.tanase2121@gmail.com>
2024-04-27 12:30:20 +0300

    stripe api key removed from visible code

--- backend/PresentationLayer/Controllers/PaymentController.cs
+++ backend/PresentationLayer/Controllers/PaymentController.cs
@@ -12,30 +12,37 @@ namespace PresentationLayer.Controllers
     public class PaymentsController : ControllerBase
     {
         private readonly IPaymentService _paymentService;
+        private readonly string _stripeApiKey;
 
-        public PaymentsController(IPaymentService paymentService)
+        public PaymentsController(IPaymentService paymentService, string stripeApiKey)
         {
             _paymentService = paymentService;
+            _stripeApiKey = stripeApiKey;
+        }
+
+        [HttpGet]
+        public IActionResult TestApiKey()
+        {
+            return Ok(_stripeApiKey);
         }
 
         [HttpPost]
         public async Task<IActionResult> ProcessPayment([FromBody] PaymentRequest paymentRequest)
         {
-            Console.WriteLine("Da ba am intrat");
             var paymentResult = await _paymentService.ProcessPaymentAsync(paymentRequest);
 
             if (paymentResult.IsSuccess)
             {
-                return Ok(new { Message = paymentResult.Message });
+                return Ok(new { message = paymentResult.Message });
             }
 
-            return StatusCode(500, new { Message = paymentResult.Message });
+            return StatusCode(500, new { message = paymentResult.Message });
         }
         
         [HttpPost("create-payment-method")]
         public async Task<IActionResult> CreatePaymentMethod([FromBody] CreatePaymentMethodRequest request)
         {
-            StripeConfiguration.ApiKey = "sk_test_51NU9A1E7blOkcXvgGAvXPPcQ8Geoz8x11TKzh21VQf1geJIqtasUDbpd58xDrYkSmnu7a1ADr6K7bBoGUKGvrzgk00eYDT1y4o"; // Replace with your actual Stripe API key
+            StripeConfiguration.ApiKey = _stripeApiKey;
 
             var options = new PaymentMethodCreateOptions
             {
